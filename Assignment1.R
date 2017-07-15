b4dae21f5818d467ad8cf564498b87250f15e198
sharadsubhedar <sharad.subhedar26@gmail.com>
2017-07-15 19:37:12 +0530

    Add files via upload

---
+++ Assignment1.R
@@ -0,0 +1,18 @@
+#print("Solution for (1)")
+seq(1, 100, 2) 
+#print("Soltion for(2)")
+x= c(1,2,3,4,5,8,6,2,11) 
+print(x) 
+#print("Solution of (3)")
+myMatrix = matrix(x,3,ncol=3) 
+print(myMatrix) 
+#print("Solution of (4)")
+a<-c(NA,11:15,NA,NA) 
+myVar= mean(a,na.rm=TRUE) 
+print(myVar) 
+#print("Solution of (5)")
+z=c("apple","banana","grape") 
+sub('a','$',z); 
+
+
+
